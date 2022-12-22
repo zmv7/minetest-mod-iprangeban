1103ab99185345a2e670057eeeefd5ad8a0d78c1
Zemtzov7 <72821250+zmv7@users.noreply.github.com>
2022-12-22 21:06:13 +0500

    description providing

--- init.lua
+++ init.lua
@@ -4,8 +4,11 @@ core.register_on_prejoinplayer(function(name,ip)
 	local stor_table = stor:to_table()
 	local list = stor_table and stor_table.fields
 	if not (name and ip and list) then return end
-	for pat,_ in pairs(list) do
+	for pat,descr in pairs(list) do
 		if ip:match(pat) then
+			if descr and descr ~= "dummy" then
+				pat = pat..". Reason: "..descr
+			end
 			return "Your IP ("..ip..") included in banned IP range: "..pat
 		end
 	end
@@ -14,23 +17,31 @@ end)
 core.register_chatcommand("ipban",{
   description = "IP range banner",
   privs = {server=true},
-  params = "<add|rm> <IP part> | <ls>",
+  params = "<add|rm> <IP part> [description] | <ls>",
   func = function(name,param)
-	local action, ip = param:match("^(%S+) (.+)$")
-	if not (action and ip) then
+	local action, ip_descr = param:match("^(%S+) (.+)$")
+	if not (action and ip_descr) then
 		action = param
 		if action == "ls" then
 			local stor_table = stor:to_table()
 			local list = stor_table and stor_table.fields
 			if not list then return end
 			local out = {}
-			for pat,_ in pairs(list) do
+			for pat,descr in pairs(list) do
+				if descr and descr ~= "dummy" then
+					pat = pat.."("..descr..")"
+				end
 				table.insert(out, pat)
 			end
 			table.sort(out)
 			return true, "List of patterns: "..table.concat(out,", ")
 		end
 		return false, "Invalid params"
+	end
+	local ip, descr = ip_descr:match("^(%S+) (.+)$")
+	if not (ip and descr) then
+		ip = ip_descr
+		descr = "dummy"
 	end
 	local segs = ip:split(".")
 	local pattern = {"%d+","%d+","%d+","%d+"}
@@ -42,7 +53,7 @@ core.register_chatcommand("ipban",{
 	end
 	local patstring = table.concat(pattern,".")
 	if action == "add" then
-		stor:set_string(patstring,"true")
+		stor:set_string(patstring,descr)
 		return true, "Added pattern: "..patstring
 	end
 	if action == "rm" then
